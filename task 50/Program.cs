78671af34d9abce3dbfa7e624957fff184bdb0a1
Alexandr Nevodov <nevodoff@gmail.com>
2022-12-17 21:51:11 +0300

    edit

--- task 50/Program.cs
+++ task 50/Program.cs
@@ -13,40 +13,72 @@
 
 Console.Clear();
 
-Console.WriteLine("Введите размеры матрицы");
+// Console.WriteLine("Введите размеры матрицы");
 
-Console.Write("Введите m: ");
-int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrix = new int[m, n];
+// int[,] matrix = new int[m, n];
 
-for (int i = 0; i < m; i++)
-   {
-       for (int j = 0; j < n; j++)
+// for (int i = 0; i < m; i++)
+//    {
+//        for (int j = 0; j < n; j++)
+//         {
+//             matrix[i, j] = new Random().Next(-10, 11);
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//    }
+// Console.WriteLine();
+
+// Console.Write("Введите координаты a:");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координаты b:");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// // if (a>m || b>n) // || a < 0 || b<0 )
+// // Console.WriteLine("такого числа нет");
+// //      else
+// //          {
+// //              Console.WriteLine(matrix[a,b]);
+// //          }
+
+// if (a<=m && b<=n && a>=0 && b>=0)   
+//     Console.WriteLine(matrix[a,b]);
+// else Console.WriteLine("такого числа нет");
+
+void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = new Random().Next(-10, 11);
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
             Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
-   }
-Console.WriteLine();
+    }
+}
 
-Console.Write("Введите координаты a:");
-int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите координаты b:");
-int b = Convert.ToInt32(Console.ReadLine());
+void ReleaseMatrix(int[, ] matrix, int x, int y)
+{
+    if (x >= matrix.GetLength(0) || y >= matrix.GetLength(1) || x < 0 || y < 0)
+        Console.WriteLine("К сожалению, Вы ошиблись в координатах");
+    else
+        Console.WriteLine(matrix[x, y]);
+}
 
-// if (a>m || b>n) // || a < 0 || b<0 )
-// Console.WriteLine("такого числа нет");
-//      else
-//          {
-//              Console.WriteLine(matrix[a,b]);
-//          }
 
-if (a<=m && b<=n && a>=0 && b>=0)   
-    Console.WriteLine(matrix[a,b]);
-else Console.WriteLine("такого числа нет");
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[, ] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.Write("Введите позицию элемента ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+ReleaseMatrix(matrix, coord[0] - 1, coord[1] - 1);
--- task 52/Program.cs
+++ task 52/Program.cs
@@ -7,38 +7,75 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 Console.Clear();
 
-Console.WriteLine("Введите размеры матрицы");
+// Console.WriteLine("Введите размеры матрицы");
 
-Console.Write("Введите m: ");
-int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrix = new int[m, n];
+// int[,] matrix = new int[m, n];
 
-for (int i = 0; i < m; i++)
-   {
-       for (int j = 0; j < n; j++)
+// for (int i = 0; i < m; i++)
+//    {
+//        for (int j = 0; j < n; j++)
+//         {
+//             matrix[i, j] = new Random().Next(0, 11);
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//    }    
+
+// int summ = 0;
+// int Summa (int[,] matrix)//, int row )
+// {
+
+// for ( int i = 0; i < m; i++)
+    
+//     summ = summ + matrix[i,0];
+// return summ;
+// //Console.WriteLine(result);
+
+// }
+
+// Summa(matrix);
+// double result = summ/m;
+// Console.Write($"Арифметическое столбца {m} - " + result); /// ТУТ Я ЗАСТРЯЛ :)
+// --------------------------------------------
+
+
+void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = new Random().Next(0, 11);
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
             Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
-   }    
-
-int summ = 0;
-int Summa (int[,] matrix)//, int row )
-{
-
-for ( int i = 0; i < m; i++)
-    
-    summ = summ + matrix[i,0];
-return summ;
-//Console.WriteLine(result);
-
+    }
 }
 
-Summa(matrix);
-double result = summ/m;
-Console.Write($"Арифметическое столбца {m} - " + result);
+
+void ReleaseMatrix(int[, ] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double summa = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            summa += matrix[i, j];
+        }
+        Console.WriteLine($"Столбец {j + 1} = {summa / matrix.GetLength(0)}");
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[, ] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+ReleaseMatrix(matrix);
